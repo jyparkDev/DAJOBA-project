5f09b47a16e8584eb1a268b9b7291c9c67749ca6
jypark <pjjy92@gmail.com>
2022-06-05 01:01:51 +0900

    DAJOBA-project-1 Issue Test

--- dajoba/src/main/java/com/kic/project/controller/TestClass.java
+++ dajoba/src/main/java/com/kic/project/controller/TestClass.java
@@ -2,6 +2,6 @@ package com.kic.project.controller;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("testdsfs");
     }
 }
